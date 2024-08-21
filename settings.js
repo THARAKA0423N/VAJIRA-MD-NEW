e9e9c441c6b083f634ac4505bfc66bcf12f8567a
THARAKA0423N <tharakadilshan499@gmail.com>
2024-08-21 13:08:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,11 +11,11 @@ const FOOTERNAME = '> NURO -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94760534874' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '20_8_63f5_8f3b_103b ' ,
+SESSION_ID: process.env.SESSION_ID || ' VAJIRA-MD=PNt0SBoC#O0wg6evGjOadtuwwydgqhANe7zveiPvBgleDrXGgVOw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94760534874" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+OWNER_NAME: process.env.OWNER_NAME || 'NURO' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
